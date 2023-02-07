c585c12430aab710addaab77bde39569ad0a7dc6
Visinho <Elvisvisinho@rocketmail.com>
2023-02-07 05:40:12 +0100

    Finished the first section of my E-commerce application

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import Clips from './utils/Clips'
+import SocialLinks from './utils/SocialLinks'
 
 const Hero = ({ visinhoapi: {title, subtitle, btntext, img, sociallinks, videos} }) => {
-    // console.log(visinhoapi);
+    
   return (
     <>
         <div className='relative h-auto w-auto flex flex-col'>
@@ -11,14 +13,29 @@ const Hero = ({ visinhoapi: {title, subtitle, btntext, img, sociallinks, videos}
                        <h1 className='text-6xl lg:text-5xl md:text-4xl sm:text-3xl xsm:text-2xl font-extrabold filter drop-shadow-sm text-slate-200'>{title}</h1> 
                        <h1 className='text-6xl lg:text-5xl md:text-4xl sm:text-3xl xsm:text-2xl font-extrabold filter drop-shadow-sm text-slate-200'>{subtitle}</h1> 
                        <button type='button' className='button-theme bg-slate-200 shadow-slate-200 rounded-xl my-5'>{btntext}</button>
-                       {/* <div></div>
-                       <div></div> */}
+                       <div className='grid items-center gap-5 md:gap-3 absolute top-[33vh] lg:top-[27vh] left-[11%] xl:left-0 w-auto h-auto'>
+                        {videos?.map((val, i) => (
+                            <Clips 
+                                key={i}
+                                imgsrc={val.imgsrc}
+                                clip={val.clip}
+                            />
+                       ))}
+                       </div>
+                       <div className='grid items-center absolute top-[33vh] lg:top-[27vh] right-0 gap-3'>
+                        {sociallinks.map((val, i) => (
+                            <SocialLinks
+                            key={i}
+                            icon={val.icon}
+                            />
+                        ))}
+                       </div>
                    </div>  
-                   <div className='w-auto h-[45vh] lg:h-[35vh] md:h-[31vh] sm:h-[21vh] xsm:h-[19vh] transitions-theme -rotate-[25deg] hover:rotate-0 cursor-pointer object-fill'>
+                   <div className=''>
                       <img 
                         src={img}
                         alt="Visinho-img/img"
-                        className=''
+                        className='w-auto h-[45vh] lg:h-[35vh] md:h-[31vh] sm:h-[21vh] xsm:h-[19vh] transitions-theme -rotate-[25deg] hover:rotate-0 cursor-pointer object-fill'
                       />  
                    </div> 
                 </div>
