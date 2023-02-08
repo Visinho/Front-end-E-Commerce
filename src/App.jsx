50cf20868e0d1f231060cfd98471065022469af2
Visinho <Elvisvisinho@rocketmail.com>
2023-02-08 11:49:37 +0100

    Complete design of some extra components of the webpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Hero, Sales } from './components';
-import { visinhoapi, popularsales, topratesales } from "./data/data.js";
+import { FlexContent, Hero, Sales } from './components';
+import { visinhoapi, popularsales, topratesales, sneaker, highlight } from "./data/data.js";
 
 
 const App = () => {
@@ -9,7 +9,9 @@ const App = () => {
       <main className='flex flex-col gap-16'>
         <Hero visinhoapi={visinhoapi}/>
         <Sales endpoint={popularsales} ifExists/>
+        <FlexContent endpoint={highlight} ifExists/>
         <Sales endpoint={topratesales}/>
+        <FlexContent endpoint={sneaker}/>
       </main>
     </>
   )
