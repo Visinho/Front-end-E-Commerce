885bd82f63365b1569705d5e5311c5e38f62edfd
Visinho <Elvisvisinho@rocketmail.com>
2023-02-10 14:04:00 +0100

    Cart components designs

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FlexContent, Footer, Hero, Navbar, Sales, Stories } from './components';
+import { FlexContent, Footer, Hero, Navbar, Sales, Stories, Cart } from './components';
 import { visinhoapi, popularsales, topratesales, sneaker, highlight, story, footerAPI } from "./data/data.js";
 
 
@@ -7,6 +7,7 @@ const App = () => {
   return (
     <>
         <Navbar/>
+        <Cart/>
       <main className='flex flex-col gap-16'>
         <Hero visinhoapi={visinhoapi}/>
         <Sales endpoint={popularsales} ifExists/>
